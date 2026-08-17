6882a5f4703973e20293e9518b5d9c16a1d5699e
DINESHREDDY1504 <gandagaridineshreddy1432@gmail.com>
2026-08-17 21:51:21 +0530

    Initialize DataWarehouse database and schemas

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,1 +1,8 @@
+-- /*Creating the database and bronze and silver and gold layers for the database name DataWarehouse*\
 
+
+create database DataWarehouse;
+use DataWarehouse;
+create schema bronze;
+create schema silver;
+create schema gold;
